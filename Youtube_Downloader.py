dbde25a79765fb62ff4a099d91a1efc7e6f56e35
m-itconsult <jchoro74@gmail.com>
2023-03-11 09:28:41 +0100

    update

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,20 +1,38 @@
+'''Download link list'''
 # Pytube doc
 # https://pytube.io/en/latest/user/streams.html#filtering-for-mp4-streams
 
 from pytube import YouTube
-print('1. Video')
-print('2. Audio')
-choice = int(input('Make your choice: '))
+import csv
 
-if (choice == 1):
-    link = str(input('Link: '))
-    yt = YouTube(link)
-    streams = yt.streams
-    stream = yt.streams.get_by_itag(22) # The itag 22 = .mp4 720p founded into the pytube doc
-    stream.download()
+links = []
+with open('links.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        links.append(row[0])
 
-if (choice == 2):
-    link = str(input('Link: '))
-    yt = YouTube(link)
-    stream = yt.streams.get_by_itag(251) # The itag 251 = .webm founded into the pytube doc
-    stream.download()
+for link in links:
+    try:
+        yt = YouTube(link)
+        stream = yt.streams.get_by_itag(251) # The itag 251 = .webm founded into the pytube doc
+        stream.download()
+    except:
+        print(f'Error downloading {link}')
+        
+''' One Download Link '''
+# print('1. Video')
+# print('2. Audio')
+# choice = int(input('Make your choice: '))
+
+# if (choice == 1):
+#     link = str(input('Link: '))
+#     yt = YouTube(link)
+#     streams = yt.streams
+#     stream = yt.streams.get_by_itag(22) # The itag 22 = .mp4 720p founded into the pytube doc
+#     stream.download()
+
+# if (choice == 2):
+#     link = str(input('Link: '))
+#     yt = YouTube(link)
+#     stream = yt.streams.get_by_itag(251) # The itag 251 = .webm founded into the pytube doc
+#     stream.download()
